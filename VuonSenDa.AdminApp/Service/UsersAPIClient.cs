eaec6944542a60436feb2cbe184086631c0ca776
daoquockhanh280998 <daoquockhanh280998@gmail.com>
2020-10-16 22:54:31 +0700

    Login AdminApp

---
+++ VuonSenDa.AdminApp/Service/UsersAPIClient.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using VuonSenDa.ViewModels.System.Users;
+
+namespace VuonSenDa.AdminApp.Service
+{
+    public class UsersAPIClient : IUsersAPIClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UsersAPIClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<string> Authenticate(LoginRequest request)
+        {
+            var json = JsonConvert.SerializeObject(request);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("https://localhost:5001");
+            var response = await client.PostAsync("/api/users/authenticate", httpContent);
+            var token = await response.Content.ReadAsStringAsync();
+            return token;
+        }
+    }
+}
